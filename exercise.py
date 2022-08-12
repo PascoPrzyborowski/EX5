7c2ce50832f1eef3251b5095f643f490e8cf7b1a
PascoPrzyborowski <unity01@gmx.net>
2022-08-12 12:13:09 +0200

    Ex5

--- exercise.py
+++ exercise.py
@@ -1,3 +1,48 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
+
+
+# Task 1
+for i in range(2):
+    print()
+    print("Task1")
+    print()
+    print("Give me three Numbers to add:" )
+    print()
+    num1 = int(input("Give me number one: "))
+    print()
+    num2 = int(input("Give me number two: "))
+    print()
+    num3 = int(input("Give me number three: "))
+    print()
+    print("Your Number one is: ", num1)
+    print()
+    print("Your Number two is: ", num2)
+    print()
+    print("Your Number three is: ", num3)
+    print()
+    a = (num1, num2, num3)
+    result = sum(a)
+    print("Your Sum is: ", result )
+    if num1 == num2 and num2 == num3 and num3 == num1:
+        #trpresult = (result*3)
+        print("tripple is: ", result*3)
+
+
+# Task 2
+
+
+# Task 3
+
+# Task 4
+
+# Task 5
+
+# Task 6
+
+# Task 7
+
+# Task 8
+
+
