e6149da98eb59db555f5fd5a1bd49085a5977120
PascoPrzyborowski <unity01@gmx.net>
2022-08-12 16:21:27 +0200

    Ex5

--- exercise.py
+++ exercise.py
@@ -4,45 +4,148 @@
 
 
 # Task 1
-for i in range(2):
-    print()
-    print("Task1")
-    print()
-    print("Give me three Numbers to add:" )
-    print()
-    num1 = int(input("Give me number one: "))
-    print()
-    num2 = int(input("Give me number two: "))
-    print()
-    num3 = int(input("Give me number three: "))
-    print()
-    print("Your Number one is: ", num1)
-    print()
-    print("Your Number two is: ", num2)
-    print()
-    print("Your Number three is: ", num3)
-    print()
-    a = (num1, num2, num3)
-    result = sum(a)
-    print("Your Sum is: ", result )
-    if num1 == num2 and num2 == num3 and num3 == num1:
-        #trpresult = (result*3)
-        print("tripple is: ", result*3)
+
+# for i in range(2):
+#     print()
+#     print("Task1")
+#     print()
+#     print("Give me three Numbers to add:" )
+#     print()
+#     num1 = int(input("Give me number one: "))
+#     print()
+#     num2 = int(input("Give me number two: "))
+#     print()
+#     num3 = int(input("Give me number three: "))
+#     print()
+#     print("Your Number one is: ", num1)
+#     print()
+#     print("Your Number two is: ", num2)
+#     print()
+#     print("Your Number three is: ", num3)
+#     print()
+#     a = (num1, num2, num3)
+#     result = sum(a)
+#     print("Your Sum is: ", result )
+#     if num1 == num2 and num2 == num3 and num3 == num1:
+#         #trpresult = (result*3)
+#         print("tripple is: ", result*3)
 
 
 # Task 2
 
+# print()
+# print("Task2")
+# # print()
+# num1 = int(input("First number: "))
+# num2 = int(input("Second number: "))
 
+# result = abs(num1-num2)
+
+# if num1 > num2:
+#     print(f"The result is: {result*2}")
+# else:
+#         print(f"The result is: {result}") 
+
+# firstnum = int(input("First num: "))
+# secnum = int(input("Second num: "))
+     
+# if firstnum > secnum:
+#     print(f"The result is {(firstnum - secnum)*2}")
+#     elif secnum >= firstnum:
+#         print(f"The result is {abs(firstnum)}")
 # Task 3
+
+# print()
+# print("Task3")
+# print()
+# for i in range(2):
+#     num1 = int(input("Give me a Number please: "))
+#     print("Number to check is:", num1)
+#     if num1 %2 != 0:
+#         print(num1, "is an odd number!")
+#     else:
+#         print(num1, "is an even number!")
 
 # Task 4
 
+# import math
+# from math import pi
+# print()
+# print("Task4")
+# print()
+# radius = float(input("Give me a number for Radius: "))
+# print(radius)
+# area = pi*radius*radius
+# x = round(area, 2)
+# print("The area of the circle with radius:", radius, "is: " , x)
+
+
+
 # Task 5
+#import random
+
+# print()
+# print("Task5")
+# print()
+
+#num = 10
+#print(random.randint(1, 10))
+
+# x = (random.randint(1, 10))
+# n = False
+# while n != True:
+#     print("Guess a number between 1 and 10 until you get it right!")
+#     guess = int(input("your Guess:" ))
+#     if guess == x:
+#         n = True
+#         print("Well done!")
+#         print("Finished!")
+#         break
 
 # Task 6
 
+# print()
+# print("Task6")
+# print()
+
+
 # Task 7
 
+# print()
+# print("Task7")
+# print()
+a = "*"
+count = 1
+countd = 6
+#pfeilbild
+
+# for i in range(6):
+#     b = count * a
+#     print(b)
+#     count = count + 1
+#     #for i in range(6):
+# while countd != 0:
+#     b = countd * a
+#     print(b)
+#     countd = countd - 1
+
+#opposit
+
+# for i in range(6):
+#     c = count * a
+#     print(c)
+#     count = count + 1
+#     #for i in range(6):
+#     while countd != 0:
+#         c = countd * a
+#         print(c)
+#         countd = countd - 1
+
+
 # Task 8
+
+# print()
+# print("Task8")
+# print()
 
 
